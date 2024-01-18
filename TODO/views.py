24af571328df825055764e98540bd274a8d2478e
yxsh <yxsh@kali-linux>
2024-01-18 20:00:21 +0530

    todo app final

--- TODO/views.py
+++ TODO/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 from TODO_APP.models import task
 def home(request):
     tasks = task.objects.filter(is_completed=False).order_by('-updated_at')
+
+    completed_tasks = task.objects.filter(is_completed=True)
+
     context = {
         'tasks':tasks,
+        'completed_tasks':completed_tasks,
     }
     return render(request, 'home.html',context)
--- TODO_APP/views.py
+++ TODO_APP/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import task
 def addTask(request):
     task1 = request.POST['task']
     task.objects.create(task=task1)
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    task1 = get_object_or_404(task, pk=pk)
+    task1.is_completed = True
+    task1.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task1 = get_object_or_404(task, pk=pk)
+    task1.is_completed = False
+    task1.save()
+    return redirect('home')
+
+def edit_task(request, pk):
+    get_task =get_object_or_404(task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task,
+        }
+        return render(request, 'edit_task.html',context)
+
+def delete_task(request, pk):
+    task1 = get_object_or_404(task, pk=pk)
+    task1.delete()
     return redirect('home')
